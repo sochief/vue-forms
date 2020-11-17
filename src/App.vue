9105246cf2431c10984facd2f488855bc5c7efdc
sochief <shafkatoo@gmail.com>
2020-11-17 14:47:10 +0300

    password validation forms

--- src/App.vue
+++ src/App.vue
@@ -18,22 +18,62 @@
           This field should be filled with email
         </div>
       </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          :class="{ 'is-invalid': $v.password.$error }"
+          @blur="$v.password.$touch()"
+          v-model="password"
+        />
+        <div class="invalid-feedback" v-if="!$v.password.minLegth">
+          Min length of password is {{ $v.password.$params.minLength.min }}. Now
+          it is {{ password.length }}
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="password">Confirm password</label>
+        <input
+          type="password"
+          id="confirm"
+          class="form-control"
+          :class="{ 'is-invalid': $v.confirmPassword.$error }"
+          @blur="$v.confirmPassword.$touch()"
+          v-model="confirmPassword"
+        />
+        <div class="invalid-feedback" v-if="!$v.confirmPassword.sameAs">
+          Passwords do not match
+        </div>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-import { required, email } from "vuelidate/lib/validators";
+import { required, email, minLength, sameAs } from "vuelidate/lib/validators";
 export default {
   data() {
     return {
       email: "",
+      password: "",
+      confirmPassword: "",
     };
   },
   validations: {
     email: {
       required,
       email,
+    },
+    password: {
+      minLength: minLength(6),
+    },
+    confirmPassword: {
+      // sameAs: sameAs("password"),
+      sameAs: sameAs((vue) => {
+        return vue.password;
+      }),
     },
   },
 };
